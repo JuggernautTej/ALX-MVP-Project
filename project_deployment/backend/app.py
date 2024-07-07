dfd94297e61d9208f93605bd283f213790388500
JuggernautTej <tiwatej@gmail.com>
2024-07-07 15:40:11 +0100

    edited app file for deployment

--- project_deployment/backend/app.py
+++ project_deployment/backend/app.py
@@ -2,15 +2,15 @@
 """This script initializes the Flask application"""
 
 from flask import Flask
+from flask_cors import CORS
 from flask_restful import Api
 from routes import initialize_routes, initialize_static_routes
 from config import Config
 import os
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
-
-os.environ['NEWS_API_KEY'] = os.getenv('NEWS_API_KEY', 'f26b66c5c8cd4b8c92b24c71f0bcf4f4')
 
 app.config.from_object(Config)
 
@@ -18,4 +18,4 @@ initialize_routes(api)
 initialize_static_routes(app)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT',5000)), debug=True)
